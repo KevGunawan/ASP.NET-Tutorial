9b4960f3e3e834b2e9508712e036a63b347e04b1
KevGunawan <gunawan.kevin6@gmail.com>
2023-11-06 19:47:09 -0500

    initial commit

---
+++ JokesWebApplication/Models/Joke.cs
@@ -0,0 +1,18 @@
+﻿namespace JokesWebApplication.Models
+{
+    public class Joke
+    {
+        public int jokeID { get; set; }
+
+        public string jokeQuestion { get; set; }
+
+        public string jokeAnswer { get; set; }
+
+        public string jokeAuthor { get; set;}
+
+        public Joke()
+        {
+                
+        }
+    }
+}
